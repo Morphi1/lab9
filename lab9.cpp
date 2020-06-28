08f9131b0ddadb52444e63a25a763cd410e8c214
Morphi1 <67324136+Morphi1@users.noreply.github.com>
2020-06-28 17:15:35 +0300

    Функция ввода и вывода динамического массива

---
+++ lab9.cpp
@@ -0,0 +1,28 @@
+#include <fstream> 
+#include <iostream> 
+#include <locale.h> 
+#include <stdlib.h>
+#include <locale.h>
+using namespace std;
+void output(int** p, int n)
+{
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < n; j++)
+			cout << p[i][j] << " ";
+	};
+}
+void input(int** p, int n)
+{
+	cout << "¬вод массива" << endl;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> p[i][j];
+}
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	return 0;
